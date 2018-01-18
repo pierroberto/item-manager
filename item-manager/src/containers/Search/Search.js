ea9dffb4ac51893ab9f1efca80e393b5a021c917
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-18 18:55:42 +0100

    Style: design the dropdown menu of the Search component

--- item-manager/src/containers/Search/Search.js
+++ item-manager/src/containers/Search/Search.js
@@ -2,25 +2,39 @@ import React from "react";
 import { connect } from "react-redux";
 import Dropdown from "react-dropdown";
 import "./search.css";
+import "./dropdown.css";
 import Dashboard from "../../components/Dashboard/Dashboard";
 
-const options = ["title", "price"];
-const defaultOption = options[0];
+const options = ["Select an option", "title", "price"];
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: options[0]
+    };
+    this._onSelect = this._onSelect.bind(this);
+  }
+
+  _onSelect(option) {
+    console.log("you sele", option.label);
+    this.setState({ selected: option });
+  }
+
   render() {
-    console.log("rendering ...", this.props);
+    const defaultOption = this.state.selected;
     return (
       <div className="search">
         <Dashboard />
-        <div className="">Search does work</div>
-        <Dropdown
-          className="search__dropdown"
-          options={options}
-          onChange={this._onSelect}
-          value={defaultOption}
-          placeholder="Select an option"
-        />
+        <div className="search__container">
+          <Dropdown
+            className="search__dropdown"
+            options={options}
+            onChange={this._onSelect}
+            value={defaultOption}
+            placeholder="Select an option"
+          />
+        </div>
       </div>
     );
   }
