51bfaff18b0be4a4ccffc0ca1690bcf895d43dd4
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-19 11:51:23 +0100

    Feat: load data in ItemView component

--- item-manager/src/components/ItemView/ItemView.js
+++ item-manager/src/components/ItemView/ItemView.js
@@ -5,7 +5,26 @@ import "./itemview.css";
 class ItemView extends React.Component {
   render() {
     console.log("props", this.props);
-    return <div className="itemview">works</div>;
+    return (
+      <div className="itemview">
+        <div className="itemview__container">
+          <div className="itemview__wrapper">
+            <div className="itemview__picture">
+              <img src={require(`../../assets/${this.props.picture}`)} />
+            </div>
+            <div className="itemview__info">
+              <div className="itemview__title">{this.props.title}</div>
+              <div className="itemview__description">
+                {this.props.description}
+              </div>
+              <div className="itemview__price">{this.props.price}</div>
+              <div className="itemview__email">{this.props.email}</div>
+            </div>
+          </div>
+          <div className="itemview__btn" />
+        </div>
+      </div>
+    );
   }
 }
 
