da6476b76b3d983a5ea00e50a8e15a03d5c3ae22
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-19 16:51:26 +0100

    Style: design ItemView component, almost complete for desktop and mobile

--- item-manager/src/components/ItemView/ItemView.js
+++ item-manager/src/components/ItemView/ItemView.js
@@ -8,21 +8,30 @@ class ItemView extends React.Component {
     return (
       <div className="itemview">
         <div className="itemview__container">
-          <div className="itemview__wrapper">
-            <div className="itemview__picture">
-              <img src={require(`../../assets/${this.props.picture}`)} />
-            </div>
-            <div className="itemview__info">
-              <div className="itemview__title">{this.props.title}</div>
-              <div className="itemview__description">
-                {this.props.description}
+          <div className="itemview__content">
+            <div className="itemview__wrapper">
+              <div className="itemview__picture">
+                <img
+                  className="itemview__picture"
+                  src={require(`../../assets/${this.props.picture}`)}
+                  alt={this.props.picture}
+                />
               </div>
-              <div className="itemview__price">{this.props.price}</div>
-              <div className="itemview__email">{this.props.email}</div>
+              <div className="itemview__info">
+                <div className="itemview__title">{this.props.title}</div>
+                <div className="itemview__description">
+                  {this.props.description}
+                </div>
+                <div className="itemview__price">
+                  Price: {this.props.price} $
+                </div>
+                <div className="itemview__email">Email: {this.props.email}</div>
+                <div className="itemview__btn" />
+              </div>
             </div>
           </div>
-          <div className="itemview__btn" />
         </div>
+        <div className="itemview__separator" />
       </div>
     );
   }
